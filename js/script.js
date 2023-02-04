03b508ba71577102cca5d1eae1e23845199c4762
ABADA <fatimaezzahra.abada@etudiant.univ-brest.fr>
2023-02-04 22:43:03 +0100

    Ajout de la fonction deplaceBall -Cette fonction met à jour la position de l'objet ballon

--- js/script.js
+++ js/script.js
@@ -8,3 +8,27 @@ function Ball(x,y){
 	this.xVel = 0;
 	this.size = 5;
 }
+
+function deplaceBall(){
+	if(ballon.yVel !== 0){
+		if(ballon.yVel > 0){
+			ballon.yVel -= ballon.decel;
+			if(ballon.yVel < 0) ballon.yVel = 0;
+		} else {
+			ballon.yVel += ballon.decel;
+			if(ballon.yVel > 0) ballon.yVel = 0;
+		}
+	}
+	if(ballon.xVel !== 0){
+		if(ballon.xVel > 0){
+			ballon.xVel -= ballon.decel;
+			if(ballon.xVel < 0) ballon.xVel = 0;
+		} else {
+			ballon.xVel += ballon.decel;
+			if(ballon.xVel > 0) ballon.xVel = 0;
+		}
+	}
+	
+	ballon.x += ballon.xVel;
+	ballon.y += ballon.yVel;
+}
